c65e6bf5c9a1a92827e9b0f7f9b3fb38bd99013d
Madhuri29c <hkullampalli@gmail.com>
2025-02-04 15:05:36 +0530

    day3

---
+++ wipro_java_project/src/com/wipro/java/exception/Exception.java
@@ -0,0 +1,20 @@
+package com.wipro.java.exception;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int a[]= {1,2,3};
+			System.out.println(a[5]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception caught: " + e.getMessage());
+		}
+		finally {
+			System.out.println("Here I am executed");
+		}
+		System.out.println("Have a look");
+	}
+
+}
